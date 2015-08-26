a7b3b73c97d8854378e4cab81f79c0bcde96bafb
tdraier <gej4quec>
2015-08-26 19:31:05 +0200

    QA-7666 : add script to setup dependsOnVisibilityOf property

---
+++ src/main/resources/jnt_bootstrapNavMenu/html/bootstrapNavMenu.cachePropertiesScript.groovy
@@ -0,0 +1,22 @@
+import org.jahia.taglibs.jcr.node.JCRTagUtils
+
+baseline = currentNode.properties['j:baselineNode']
+maxDepth = currentNode.properties['j:maxDepth']
+
+def base;
+if (!baseline || baseline.string == 'home') {
+    base = renderContext.site.home
+} else if (baseline.string == 'currentPage') {
+    base = JCRTagUtils.getMeAndParentsOfType(renderContext.mainResource.node, "jnt:page")[0]
+}
+if (!base) {
+    base = renderContext.mainResource.node
+}
+
+def v = base.path;
+
+1.upto(maxDepth.long, {
+    v += "(/[^/]+)?"
+})
+
+cacheProperties.put("cache.dependsOnVisibilityOf", v)
